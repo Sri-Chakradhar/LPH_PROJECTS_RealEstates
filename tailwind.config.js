b18395843e9e38b1bcd866816dfd13c1e02b4b53
Sri-Chakradhar <gsrichakradhar1@gmail.com>
2022-12-28 18:54:15 +0530

    dropdown complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./main/html/*.html"],
-    theme: {
-      extend: {
-        fontFamily:{
-          nunito : ['Nunito'],
-          lato : ['lato']
-        }
-      },
+  content: ["./main/html/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        nunito: ['Nunito'],
+        lato: ['lato']
+      }
     },
-    plugins: [],
-  }
-  
+  },
+  variants: {
+    extend: {
+      display: ['group-focus'],
+      opacity: ['group-focus'],
+      inset: ['group-focus']
+    },
+  },
+  plugins: [],
+}
