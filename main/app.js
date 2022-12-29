9c4220e91e3b4876f1408f4da1e7c105e679f178
Sri-Chakradhar <gsrichakradhar1@gmail.com>
2022-12-29 16:16:52 +0530

    little changes

--- main/app.js
+++ main/app.js
@@ -5,28 +5,32 @@ const bodypasser = require("body-parser");
 
 let app = express();
 
-app.use(bodypasser.urlencoded({extended:true}));
-app.use("/img",express.static("img"));
-app.use("/css",express.static("css"));
+app.use(bodypasser.urlencoded({
+    extended: true
+}));
+app.use("/img", express.static("img"));
+app.use("/css", express.static("css"));
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.sendFile(__dirname + "/html/index.html");
 });
 
-app.get("/login",(req,res)=>{
+app.get("/login", (req, res) => {
     res.sendFile(__dirname + "/html/login.html");
 });
 
-app.get("/houseandapartments",(req,res)=>{
+app.get("/houseandapartments", (req, res) => {
     res.sendFile(__dirname + "/html/house&apartments.html");
 });
 
-app.get("/landandplots",(req,res)=>{
+app.get("/landandplots", (req, res) => {
     res.sendFile(__dirname + "/html/land&plots.html");
 });
 
-app.listen(3000,()=>{
+app.get("/signin", (req, res) => {
+    res.sendFile(__dirname + "/signin.html");
+});
+
+app.listen(3000, () => {
     console.log("3000 server");
-})
-
-
+});
